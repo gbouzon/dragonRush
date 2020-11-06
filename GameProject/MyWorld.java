9fffe32732aed2d91d0a28537c49d3165b8b3255
Giuliana Bouzon <ga@bouzon.com.br>
2020-11-05 23:49:33 -0500

    Changed dimensions in the World and added a new class

--- GameProject/MyWorld.java
+++ GameProject/MyWorld.java
@@ -15,7 +15,7 @@ public class MyWorld extends World
      */
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        // Create a new world with 1000x1000 cells with a cell size of 1x1 pixels.
+        super(1000, 1000, 1); 
     }
 }
