9abe7455a4a6439397510d34c7afbca1df931f20
Giuliana Bouzon <ga@bouzon.com.br>
2020-11-12 14:52:40 -0500

    Updated classes

--- GameProject/Allies.java
+++ GameProject/Allies.java
@@ -1,16 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Fruits here.
+ * Write a description of class Allies here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Fruits extends Allies
+public class Allies extends Actor
 {
-    int random; // int to generate one of the child classes objects randomly
     /**
-     * Act - do whatever the Fruits wants to do. This method is called whenever
+     * Act - do whatever the Allies wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
--- GameProject/Coins.java
+++ GameProject/Coins.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Coins extends Actor
+public class Coins extends Allies
 {
     private GreenfootImage[] coins;
     private int index, counter;
